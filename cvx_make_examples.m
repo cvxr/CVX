a85be043982575b6d7980b94f516fc439a9843b2
Michael C. Grant <mcg@cvxr.com>
2005-12-14 01:11:06 +0000

    Finishing up

--- cvx_make_examples.m
+++ cvx_make_examples.m
@@ -61,122 +61,231 @@ for k = 1 : length( args ),
     %
     % Process the file or directory
     %   
+    
+    if isempty( file ) & strcmp( mpath, base ),
+        cd( base );
+        [ fidr, message ] = fopen( 'index.html', 'r' );
+        if fidr < 0,
+            error( 'Cannot open index.html\n   %s', message );
+        end
+        t_head = fread( fidr, Inf, 'uint8=>char' )';
+        t_tail = strfind( t_head, '<!-- END TOC -->' );
+        if length( t_tail ) ~= 1
+            error( 'Corrupt index.html file, cannot proceed.' );
+        end
+        t_tail = t_head( t_tail : end );
+        n_head = strfind( t_head, '<!-- BEGIN TOC -->' );
+        if length( n_head ) ~= 1,
+            error( 'Corrupt index.html file, cannot proceed.' );
+        end
+        t_head( n_head + length( '<!-- BEGIN TOC -->' ) + 1 : end ) = [];
+        fclose( fidr );
+        [ fidw, message ] = fopen( 'index.html.new', 'w+' );
+        if fidw < 0,
+            error( 'Cannot open index.html.new\n   %s', message );
+        end
+        fwrite( fidw, t_head, 'char' );
+        fprintf( fidw, '<h3>Last updated: %s</h3>\n', date );
+    else,
+        fidw = -1;
+    end
 
-    try,
+%    try,
         if isempty( file ),
-            generate_directory( mpath, '', force );
+            generate_directory( mpath, '', force, fidw, base );
         else,
             cd( mpath );
             generate_file( file, '', force );
         end
         cd( odir );
-    catch,
-        err = lasterror;
-        cd( odir );
-        rethrow( err );
+%    catch,
+%        err = lasterror;
+%       cd( odir );
+%        if fidw >= 0,
+%            fclose( fidw );
+%            delete index.html.new
+%        end
+%        rethrow( err );
+%    end
+
+    if fidw >= 0,
+        fwrite( fidw, t_tail, 'char' );
+        fclose( fidw );
+        cd( mpath )
+        compare_and_replace( '', 'index.html' );
     end
     
+
 end
 
-function title = generate_directory( mpath, prefix, force )
+function [ title, files ] = generate_directory( mpath, prefix, force, fidc, base )
 
 disp( sprintf( '%sDirectory: %s', prefix, mpath ) );
 prefix = [ prefix, '   ' ];
 cd( mpath );
 mpath = cd;
-names = dir;
-mlen  = 0;
-isdir = [ names.isdir ];
-dates = { names.date };
-names = { names.name };
+
+%
+% Open Contents.m file and retrieve title and comments
+%
+
+title = '';
+comments = {};
+[ fidr, message ] = fopen( 'Contents.m', 'r' );
+if fidr >= 0,
+    temp = fgetl( fidr );
+    if length( temp ) > 2 & temp( 1 ) == '%' & temp( 2 ) == ' ' & temp( 3 ) ~= ' ',
+        title = temp( min( find( temp ~= '%' & temp ~= ' ' ) ) : end );
+        while ~feof( fidr ),
+            temp = fgetl( fidr );
+            if temp( 1 ) ~= '%' | ~any( temp ~= '%' & temp ~= ' ' ), break; end
+            comments{end+1} = temp( min( find( temp ~= '%' & temp ~= ' ' ) ) : end );
+        end
+    end
+    fclose( fidr );
+elseif ~isempty( dir( 'Contents.m' ) ),
+    error( 'Cannot open Contents.m for reading\n   %s', message );
+end
+
+%
+% Open Comments.m.new and deposit title and comments
+%
+
+[ fidw, message ] = fopen( 'Contents.m.new', 'w+' );
+if fidw < 0,
+    if fidr >= 0, fclose( fidr ); end
+    error( 'Cannot open Contents.m.new\n   %s', message );
+elseif ~isempty( title ),
+    fprintf( fidw, '%% %s\n', title );
+    for k = 1 : length( comments ),
+        fprintf( fidw, '%% %s\n', comments{k} );
+    end
+    fprintf( fidw, '%%\n' );
+end
+
+%
+% If needed, deposit directory name, title, and comments into index.html
+%
+
+if fidc >= 0,
+    dpath = mpath( length(base) + 2 : end );
+    dpath(dpath=='\') = '/';
+    dpath(end+1) = '/';
+    if length( dpath ) > 1,
+        [ dpath2, dname ] = fileparts( mpath );
+        if isempty( title ),
+            fprintf( fidc, '<dt><a href="%s">%s/</a></dt>\n', dpath, dname ),
+        else,
+            fprintf( fidc, '<dt><b>%s</b> (<a href="%s">%s/</a>)</dt>\n', title, dpath, dname );
+        end
+        fprintf( fidc, '<dd>\n' );
+        for k = 1 : length( comments ),
+            fprintf( fidc, '%s\n', comments{k} );
+        end
+    end
+    dlstr = sprintf( '<dl>\n' );
+    fprintf( fidc, dlstr );
+end
+
+%
+% Determine the names of the files and subdirectories to process
+%
+
+dd = dir;
+dnames = {};
+fnames = {};
 files = struct( 'name', {}, 'title', {} );
-for k = 1 : length( names ),
-    name = names{k};
-    cd( mpath );
-    if isdir( k ),
-        if name(1) == '.', continue; end
-        if strcmp( name, 'html' ), continue; end
-        temp = generate_directory( name, prefix, force );
-        name( end + 1 ) = '/';
+mlen = 0;
+for k = 1 : length( dd ),
+    name = dd(k).name;
+    if dd(k).isdir,
+        if name(1) == '.' | strcmp( name, 'html' ), continue; end
+        dnames{end+1} = name;
     else,
         if length( name ) < 2 | ~strcmp( name(end-1:end), '.m' ), continue; end
         if strcmp( name, 'Contents.m' ), continue; end
-        temp = generate_file( name, prefix, force );
+        fnames{end+1} = name;
+    end
+end
+
+%
+% Process the subdirectories
+%
+
+for k = 1 : length( dnames ),
+    name = dnames{k};
+    if name(1) == '.' | strcmp( name, 'html' ), continue; end
+    [ temp, contents ] = generate_directory( name, prefix, force, fidc, base );
+    if isempty( temp ), temp = '(no title)'; end
+    name( end + 1 ) = '/';
+    files( end + 1 ) = struct( 'name', name, 'title', temp );
+    mlen = max( mlen, length( name ) );
+    cd( mpath );
+end
+
+%
+% Process the files
+%
+
+if fidc >= 0 & length( dpath ) <= 1 & ~isempty( fnames ) & ~isempty( dnames ),
+    need_misc = true;
+    fprintf( fidc, '<dt><b>Uncategorized files:</b></dt>\n' );
+    fprintf( fidc, '<dd>\n<dl>\n');
+else,
+    need_misc = false;
+end
+for k = 1 : length( fnames ),
+    name = fnames{k};
+    if length( name ) < 2 | ~strcmp( name(end-1:end), '.m' ), continue; end
+    if strcmp( name, 'Contents.m' ), continue; end
+    temp = generate_file( name, prefix, force );
+    if fidc >= 0,
+        if isempty( temp ),
+            fprintf( fidc, '<dt><a href="%s/%s">%s</a></dt>\n', dpath, name, name );
+        else,
+            fprintf( fidc, '<dt><a href="%s/html/%shtml">%s</a> (<a href="%s/%s">%s</a>)</dt>\n', dpath, name(1:end-1), temp, dpath, name, name );
+        end
     end
     if isempty( temp ), temp = '(no title)'; end
     files( end + 1 ) = struct( 'name', name, 'title', temp );
     mlen = max( mlen, length( name ) );
 end
-cd( mpath );
-title = '';
-comments = {};
-if isempty( dir( 'Contents.m' ) ),
-    fidr = -1;
-else,
-    fidr = fopen( 'Contents.m', 'r' );
+if need_misc,
+    fprintf( fidc, '</dl>\n</dd>\n' );
 end
-if fidr >= 0 | ~isempty( files ) | ~isempty( dirs ),
-    fprintf( 1, '%sContents.m ...', prefix );
-    if fidr >= 0,
-        title = fgetl( fidr );
-        if length( title ) > 2 & title( 1 ) == '%' & title( 2 ) == ' ' & title( 3 ) ~= ' ',
-            title = title( min( find( title ~= '%' & title ~= ' ' ) ) : end );
-        else,
-            title = '';
-        end
-    else,
-        title = '';
-    end
-    [ fidw, message ] = fopen( 'Contents.m.new', 'w+' );
-    if fidw < 0,
-        if fidr >= 0, fclose( fidr ); end
-        error( 'Cannot open Contents.m.new\n   %s', message );
-    end
-    if ~isempty( title ),
-        fprintf( fidw, '%% %s\n', title );
-        while ~feof( fidr ),
-            temp = fgetl( fidr );
-            if temp( 1 ) ~= '%' | ~any( temp ~= '%' & temp ~= ' ' ), break; end
-            fprintf( fidw, '%s\n', temp );
-        end
-        fprintf( fidw, '%%\n' );
-    end
-    for k = 1 : length( files ),
-        tfile = files(k);
-        tfile.name(end+1:mlen) = ' ';
-        if isempty( tfile.title ),
-            fprintf( fidw, '%%  %s - (no title)\n', tfile.name );
-        else,
-            fprintf( fidw, '%%  %s - %s\n', tfile.name, tfile.title );
-        end
-    end
-    fprintf( fidw, 'help Contents\n' );
-    fseek( fidw, 0, -1 );
-    c2 = fread( fidw, Inf, 'uint8' );
-    fclose( fidw );
-    if fidr >= 0,
-        fseek( fidr, 0, -1 );
-        c1 = fread( fidr, Inf, 'uint8' );
-        fclose( fidr );
-    else,
-        c1 = [];
-    end
-    if length( c1 ) ~= length( c2 ) | any( c1 ~= c2 ),
-        [ success, message ] = movefile( 'Contents.m.new', 'Contents.m', 'f' );
-        if ~success,
-            error( 'Cannot move Contents.m.new into place\n   %s', message );
-            delete Contents.m.new
-        end
-        if fidr >= 0,
-            fprintf( 1, ' updated.\n' );
-        else, 
-            fprintf( 1, ' created.\n' );
-        end
-    else,
-        delete Contents.m.new
-        fprintf( 1, ' up to date.\n' );
+
+%
+% Finish catalog.html.new entry
+%
+
+if fidc >= 0,
+    fprintf( fidc, '</dl>\n' );
+    if ~isempty( dpath ),
+        fprintf( fidc, '</dd>\n' );
     end
 end
+
+%
+% Finish Contents.m.new
+%
+
+for k = 1 : length( files ),
+    tfile = files(k);
+    tfile.name(end+1:mlen) = ' ';
+    if isempty( tfile.title ),
+        fprintf( fidw, '%%  %s - (no title)\n', tfile.name );
+    else,
+        fprintf( fidw, '%%  %s - %s\n', tfile.name, tfile.title );
+    end
+end
+fclose( fidw );
+
+%
+% Compare Contents.m and Contents.m.new and update if necessary
+%
+
+cd( mpath )
+compare_and_replace( prefix, 'Contents.m' );
 
 function title = generate_file( name, prefix, force )
 
@@ -260,3 +369,39 @@ end
 S = sscanf( dstr, '%d-%3s-%d %d:%d:%d' );
 S = [ S(5), find(strcmp(char(S(2:4)'),mstrs)), S(1), S(6), S(7), S(8) ];
 dnum = S(6) + 100 * ( S(5) + 100 * ( S(4) + 100 * ( S(3) + 100 * ( S(2) + 100 * S(1) ) ) ) );
+
+function compare_and_replace( prefix, oldname )
+
+names = { oldname, [ oldname, '.new' ] };
+fprintf( 1, '%s%s ... ', prefix, oldname );
+fids = [];
+c = {};
+for k = 1 : 2,
+    [ fids(k), message ] = fopen( names{k}, 'r' );
+    if fids(k) < 0 & ~isempty( dir( names{k} ) ),
+        error( 'Cannot open file %s for reading:\n   %s', names{k}, message );
+    end
+    c{k} = fread( fids(k), Inf, 'uint8' );
+    fclose( fids(k) );
+end
+if length( c{2} ) == 0,
+    if fids(k) >= 0,
+        fprintf( 1, ' removed.\n' );
+        delete( oldname );
+    end
+    delete( names{2} );
+elseif length( c{1} ) ~= length( c{2} ) | any( c{1} ~= c{2} ),
+    [ success, message ] = movefile( names{2}, names{1}, 'f' );
+    if ~success,
+        error( 'Cannot move %s into place\n   %s', names{2}, message );
+        delete( names{2} )
+    end
+    if ~isempty( c{1} ),
+        fprintf( 1, ' updated.\n' );
+    else,
+        fprintf( 1, ' created.\n' );
+    end
+else,
+    delete( names{2} )
+    fprintf( 1, ' up to date.\n' );
+end
