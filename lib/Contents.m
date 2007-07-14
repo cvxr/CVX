937712a9638b23af0394a5b8cf542695a71e32ee
Michael C. Grant <mcg@cvxr.com>
2007-07-14 19:21:20 +0000

    Contents for lib/

---
+++ lib/Contents.m
@@ -0,0 +1,4 @@
+% CVX: Internal functions and scripts.
+%    This directory contains code that is meant for internal use by
+%    the CVX system itself. Documentation of the functions in this
+%    directory is more sparse and not intended for end users.
