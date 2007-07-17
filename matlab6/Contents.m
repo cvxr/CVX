1302294fb0fbd0b01f033c4908fe862d061a0685
Michael C. Grant <mcg@cvxr.com>
2007-07-17 21:53:42 +0000

    Documentation updates

---
+++ matlab6/Contents.m
@@ -0,0 +1,7 @@
+%CVX: Matlab 6 compatability functions.
+%   This directory contains definitions for 'true' and 'false'. These
+%   functions are used extensively in CVX, and are built in for MATLAB
+%   6.5 and later. For MATLAB 6.1 we supply these surrogates. It is
+%   *very* important, however, that this directory NOT be placed in
+%   the MATLAB search path for older versions of MATLAB.
+
