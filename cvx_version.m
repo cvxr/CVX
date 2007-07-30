b9fbbab3104967ad2884994b23f726e913cff2c5
Michael C. Grant <mcg@cvxr.com>
2007-07-30 00:32:24 +0000

    version m-file

---
+++ cvx_version.m
@@ -0,0 +1,11 @@
+function [ vv, bb ] = cvx_version
+v = '___VERSION___';
+b = '___BUILD__';
+if nargout == 0,
+   disp( sprintf( 'CVX version %s (build %s)', v, b ) );
+else
+   vv = str2num( v );
+   bb = str2num( b );
+end
+
+
