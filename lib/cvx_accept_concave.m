b29c189078e818264057c17114fa478c61d7d0f2
Michael C. Grant <mcg@cvxr.com>
2008-03-06 23:27:40 +0000

    Bug fixes

--- lib/cvx_accept_concave.m
+++ lib/cvx_accept_concave.m
@@ -1,15 +1,18 @@
 function x = cvx_accept_concave( x )
-t = cvx_vexity( x ) < 0;
-if any( t( : ) ),
-    prob = cvxprob( 'current' );
-    if all( t ),
-        src = x;
-        dst = newtemp( prob, size( src ) );
-        x = dst;
-    else
-        src = x( t );
-        dst = newtemp( prob, size( src ) );
-        x( t ) = dst;
+if isa( x, 'cvx' ),
+    t = cvx_vexity( x ) < 0;
+    if any( t( : ) ),
+        global cvx___
+        prob = cvx___.problems(end).self;
+        if all( t ),
+            src = x;
+            dst = newtemp( prob, size( src ) );
+            x = dst;
+        else
+            src = x( t );
+            dst = newtemp( prob, size( src ) );
+            x( t ) = dst;
+        end
+        newcnstr( prob, src(:), dst(:), '>=' );
     end
-    newcnstr( prob, src(:), dst(:), '>=' );
 end
