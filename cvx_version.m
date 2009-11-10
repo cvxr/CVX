2fe681639c8dc1fc6f64ee181465ab34f36c22dc
Michael C. Grant <mcg@cvxr.com>
2009-11-10 19:29:47 +0000

    Version change

--- cvx_version.m
+++ cvx_version.m
@@ -1,18 +1,14 @@
-function [ vv, bb ] = cvx_version
-v = '___VERSION___';
-b = '___BUILD___';
+function [ cvx_ver, cvx_bld ] = cvx_version
+cvx_ver = 1.21;
+cvx_bld = '___BUILD___';
 if nargout == 0,
-   fprintf( 1, 'CVX version %s (build %s)\n', v, b );
+   fprintf( 1, 'CVX version %g (build %s)\n', cvx_ver, cvx_bld );
    if exist( 'OCTAVE_VERSION', 'var' ),
        fprintf( 1, 'GNU Octave %s on %s', version, computer );
    else
        verd = ver('MATLAB');
        fprintf( 1, 'MATLAB version %s %s on %s', verd.Version, verd.Release, computer );
    end
-else
-   vv = str2double( v );
-   bb = str2double( b );
 end
-
-
+cvx_bld = str2double(cvx_bld);
 
