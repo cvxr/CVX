526b6353c34a214d511e7b8c5e8df9e88788c250
Michael C. Grant <mcg@cvxr.com>
2010-05-28 20:26:16 +0000

    Updated version information

--- cvx_version.m
+++ cvx_version.m
@@ -1,10 +1,11 @@
-function [ cvx_ver, cvx_bld ] = cvx_version
+function [ cvx_ver2, cvx_bld ] = cvx_version
 cvx_ver = 1.21;
 cvx_bld = ':::BUILD:::';
 cvx_bdate = ':::BDATE:::';
 cvx_dbld = ':::DBUILD:::';
 cvx_ddate = ':::DDATE:::';
 if nargout == 0,
+   fprintf( '\n' );
    fprintf( 'CVX version %g\n', cvx_ver );
    fprintf( '    Code: build %s, %s\n', cvx_bld, cvx_bdate );
    fprintf( '    Documentation: build %s, %s\n', cvx_dbld, cvx_ddate );
@@ -15,6 +16,8 @@ if nargout == 0,
        verd = ver('MATLAB');
        fprintf( 'MATLAB version %s %s on %s\n', verd.Version, verd.Release, computer );
    end
+   fprintf( '\n' );
+else
+    cvx_ver2 = cvx_ver;
 end
-cvx_bld = str2double(cvx_bld);
 
