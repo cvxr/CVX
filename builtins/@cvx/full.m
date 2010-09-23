d22fd2817133d023cd80f322c28d6a7a24068aae
Michael C. Grant <mcg@cvxr.com>
2010-09-23 02:10:04 +0000

    full.m: added for Matlab R2010b support

---
+++ builtins/@cvx/full.m
@@ -0,0 +1,11 @@
+function z = full( x )
+
+%   Disciplined convex/geometric programming information for FULL (+):
+%      This is effectively a "no-op", and can be used in DCPs and DGPs
+%      without restrictions.
+
+z = x;
+
+% Copyright 2010 Michael C. Grant and Stephen P. Boyd.
+% See the file COPYING.txt for full copyright information.
+% The command 'cvx_where' will show where this file is located.
