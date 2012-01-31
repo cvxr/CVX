ed5dbc7e144ab5633db8c943355de5dfe72dd8fe
Michael C. Grant <mcg@cvxr.com>
2012-01-31 21:02:22 +0000

    Fix for the order-2 second order cone bug in SeDumi

--- lib/cvx_solve_sedumi.m
+++ lib/cvx_solve_sedumi.m
@@ -1,27 +1,42 @@
 function [ x, y, status, tol, iters, z ] = cvx_solve_sedumi( At, b, c, nonls, quiet, prec )
 
-n_in = 0;
-n_out = 0;
 max_sc = 0;
 n = length( c );
 m = length( b );
-K = struct( 'l', 0, 'q', [], 's', [], 'scomplex', [], 'ycomplex', [] );
-reord = struct( 'f', [], 'l', [], 'q', [], 'sr', [], 'sc', [], 'sv', [] );
+K = struct( 'f', 0, 'l', 0, 'q', [], 's', [], 'scomplex', [], 'ycomplex', [] );
+
+reord = struct( 'n', 0, 'r', [], 'c', [], 'v', [] );
+reord = struct( 'f', reord, 'l', reord, 'a', reord, 'q', reord, 's', reord, 'h', reord );
+reord.f.n = n;
 for k = 1 : length( nonls ),
     temp = nonls( k ).indices;
     nn = size( temp, 1 );
     nv = size( temp, 2 );
     nnv = nn * nv;
     tt = nonls( k ).type;
-    n_in = n_in + nnv;
-    n_out = n_out + nnv;
+    reord.f.n = reord.f.n - nnv;
     if nn == 1 || isequal( tt, 'nonnegative' ),
-        K.l = K.l + nn * nv;
-        reord.l = [ reord.l ; temp( : ) ];
+        reord.l.r = [ reord.l.r ; temp(:) ];
+        reord.l.c = [ reord.l.c ; reord.l.n + ( 1 : nnv )' ];
+        reord.l.v = [ reord.l.v ; ones(nnv,1) ];
+        reord.l.n = reord.l.n + nnv;
     elseif isequal( tt, 'lorentz' ),
-        K.q = [ K.q, nn * ones( 1, nv ) ];
-        temp = temp( [ end, 1 : end - 1 ], : );
-        reord.q = [ reord.q ; temp( : ) ];
+        if nn == 2,
+            rr = [ temp ; temp ];
+            cc = reshape( floor( 1 : 0.5 : 2 * nv + 0.5 ), 4, nv );
+            vv = [1;1;-1;1]; vv = vv(:,ones(1,nv));
+            reord.a.r = [ reord.a.r ; rr(:) ];
+            reord.a.c = [ reord.a.c ; cc(:) + reord.a.n ];
+            reord.a.v = [ reord.a.v ; vv(:) ];
+            reord.a.n = reord.a.n + nnv;
+        else
+            temp = temp( [ end, 1 : end - 1 ], : );
+            reord.q.r = [ reord.q.r ; temp(:) ];
+            reord.q.c = [ reord.q.c ; reord.q.n + ( 1 : nnv )' ];
+            reord.q.v = [ reord.q.v ; ones(nnv,1) ];
+            reord.q.n = reord.q.n + nnv;
+            K.q = [ K.q, nn * ones( 1, nv ) ];
+        end
     else
         if isequal( tt, 'semidefinite' ),
             nn = 0.5 * ( sqrt( 8 * nn + 1 ) - 1 );
@@ -37,38 +52,39 @@ for k = 1 : length( nonls ),
         stri = cvx_invert_structure( str, 'compact' )';
         [ rr, cc, vv ] = find( stri );
         rr = temp( rr );
-        max_cc = max(cc);
-        n_out = n_out + ( max_cc - nnv );
-        cc = cc + max_sc;
-        max_sc = max_sc + max_cc;
-        reord.sr = [ reord.sr; rr( : ) ];
-        reord.sc = [ reord.sc; cc( : ) ];
-        reord.sv = [ reord.sv; vv( : ) ];
+        reord.s.r = [ reord.s.r; rr( : ) ];
+        reord.s.c = [ reord.s.c; cc( : ) + reord.s.n ];
+        reord.s.v = [ reord.s.v; vv( : ) ];
+        reord.s.n = reord.s.n + size( stri, 2 );
     end
 end
 
-n_f = n - n_in;
-if n_f,
-    n_out = n_out + n_f + 1;
-    K.q(end+1) = n_f + 1;
-    reord.f = ( 1 : n )';
-    reord.f( [ reord.l ; reord.q ; reord.sr ] ) = [];
+if reord.f.n > 0,
+    reord.f.r = [ 1 : n ]';
+    reord.f.r( [ reord.l.r ; reord.a.r ; reord.q.r ; reord.s.r ] ) = [];
+    reord.f.c = ( 1 : reord.f.n )';
+    reord.f.v = ones(reord.f.n,1);
 end
-n_d = max( m - n - n_f + 1, isempty( At ) );
+n_d = max( m - n - reord.f.n + 1, isempty( At ) );
 if n_d,
-    n_out = n_out + n_d;
-    K.l = K.l + n_d;
+    reord.l.n = reord.l.n + n_d;
 end
-n_l = length(reord.l);
-n_q = length(reord.q);
-reord.sr = [ reord.l; reord.q;         reord.f;               reord.sr ];
-reord.sc = [ 1:n_l,   n_l+n_d+(1:n_q), n_l+n_d+n_q+(2:n_f+1), reord.sc'+(n_out-max_sc) ];
-reord.sv = [ ones(n_l+n_q+n_f,1);                             reord.sv ];
-reord = sparse( reord.sr, reord.sc', reord.sv, n, n_out );
+K.f = reord.f.n;
+K.l = reord.l.n + reord.a.n;
+n_out = reord.f.n;
+reord.l.c = reord.l.c + n_out; n_out = n_out + reord.l.n;
+reord.a.c = reord.a.c + n_out; n_out = n_out + reord.a.n;
+reord.q.c = reord.q.c + n_out; n_out = n_out + reord.q.n;
+reord.s.c = reord.s.c + n_out; n_out = n_out + reord.s.n;
+reord = sparse( ...
+    [ reord.f.r ; reord.l.r ; reord.a.r ; reord.q.r ; reord.s.r ], ...
+    [ reord.f.c ; reord.l.c ; reord.a.c ; reord.q.c ; reord.s.c ], ...
+    [ reord.f.v ; reord.l.v ; reord.a.v ; reord.q.v ; reord.s.v ], ...
+    n, n_out );
 
 At = reord' * At;
 c  = reord' * c;
-pars.free   = 0;
+pars.free = K.f > 1 && nnz( K.q );
 pars.eps     = prec(1);
 pars.bigeps  = prec(3);
 if quiet,
@@ -111,6 +127,11 @@ else
 end
 if info.numerr == 2,
     status = 'Failed';
+    if any( K.q == 2 ),
+        warning( sprintf( ...
+            [ 'This solver failure may possibly be due to a known bug in the SeDuMi solver.\n', ...
+              'Try switching to SDPT3 by inserting "cvx_solver sdpt3" into your model.' ] ) );
+    end
 else
     if isempty( status ),
         status = 'Solved';
