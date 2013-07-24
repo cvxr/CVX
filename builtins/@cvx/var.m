14e8fbe4293da2ab4b8668ed44ae816406c49e50
Michael C. Grant <mcg@cvxr.com>
2013-07-24 19:23:32 +0000

    New functions: var, std, avg_abs_dev, avg_abs_dev_med

---
+++ builtins/@cvx/var.m
@@ -0,0 +1,9 @@
+function y = var( varargin )
+
+%STD    Internal cvx version.
+
+y = square_pos( std( varargin{:} ) );
+
+% Copyright 2005-2013 CVX Research, Inc. 
+% See the file COPYING.txt for full copyright information.
+% The command 'cvx_where' will show where this file is located.
